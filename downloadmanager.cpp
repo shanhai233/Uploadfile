ab272f8fe17eae926b81bf6cf4ef9fcd98d3407d
Your Name <you@example.com>
2022-08-29 22:35:45 +0800

    first

---
+++ downloadmanager.cpp
@@ -0,0 +1,7 @@
+#include "downloadmanager.h"
+#include <QFile>
+#include <QDebug>
+#include <QFileInfo>
+#include <QDir>
+
+#define DOWNLOAD_FILE_SUFFIX    "_tmp"
---
+++ downloadmanager.h
@@ -0,0 +1,11 @@
+#ifndef DOWNLOADMANAGER_H
+#define DOWNLOADMANAGER_H
+
+
+class DownLoadManager
+{
+public:
+    DownLoadManager();
+};
+
+#endif // DOWNLOADMANAGER_H
